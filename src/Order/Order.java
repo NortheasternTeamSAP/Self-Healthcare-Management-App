49b8f02a69fe961c4b016cac623e104763765b0f
Sravya Sree <ogirala.s@northeastern.edu>
2021-08-17 07:31:48 -0700

    Added appointmentID attr, dosage class, prescriprion class

--- src/Order/Order.java
+++ src/Order/Order.java
@@ -40,13 +40,15 @@ public class Order extends WorkRequest {
     private String result;
     private boolean assign;
     private String orderId;
+    private String appointmentID;
 
     public Order(
             Pharmacy pharmacy, 
             Doctor doctor, 
             Patient patient,
             int quantity, 
-            Medicine medicine) {
+            Medicine medicine,
+            String appointmentID) {
         
         this.orderStatus = OrderStatus.INPROCESS;
         this.pharmacy = pharmacy;
@@ -54,6 +56,7 @@ public class Order extends WorkRequest {
         this.patient = patient;
         this.quantity = quantity;
         this.medicine = medicine;
+        this.appointmentID = appointmentID;
     }
 
     public OrderStatus getOrderStatus() {
@@ -135,5 +138,12 @@ public class Order extends WorkRequest {
     public void setOrderId(String orderId) {
         this.orderId = orderId;
     }
-    
+
+    public String getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(String appointmentID) {
+        this.appointmentID = appointmentID;
+    } 
 }
